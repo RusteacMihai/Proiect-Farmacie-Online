cb343330c162b5022fa6858a8093f7de84131b75
RusteacMihai <141055832+RusteacMihai@users.noreply.github.com>
2024-02-21 20:47:22 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
             element={<EditProduse />}
           ></Route>
 
-          <Route path="/Proiect-Farmacie-Online" element={<MainContainerGuest />} />
+          <Route basename="/Proiect-Farmacie-Online" element={<MainContainerGuest />} />
         </Routes>
       </ShopContextProvider>
     </div>
